0988872e393780d9cc93a8625a5acc0fde02d200
wangjingjing <331353149@qq.com>
2022-11-21 11:35:11 +0800

    new project

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+U
+%src/main/java/com/lionbridge/App.java,7\9\79b4c21f5b9d7d4f42396981583668cbaaee20c7
---
+++ src/main/java/com/lionbridge/App.java
@@ -0,0 +1,15 @@
+package com.lionbridge;
+
+/**
+ * @class: App
+ * @author: wangjing
+ * @date: 2022/9/5/0005
+ * @desc: TODO
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world !");
+    }
+}
