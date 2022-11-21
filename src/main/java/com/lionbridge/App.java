3a792bdb5eced1916b42f01dd617074456d152e1
wangjingjing <331353149@qq.com>
2022-11-21 19:47:46 +0800

    new submit local

--- src/main/java/com/lionbridge/App.java
+++ src/main/java/com/lionbridge/App.java
@@ -10,9 +10,9 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("cccccccccccccccc !");
-        System.out.println("bbbbbbbbbbbbbbbb !");
-        System.out.println("aaaaaaaaaaaaaaaa !");
-        System.out.println("dddddddddddddddd !");
+        System.out.println("1111111111111 !");
+        System.out.println("2222222222222 !");
+        System.out.println("3333333333333!");
+        System.out.println("4444444444444 !");
     }
 }
